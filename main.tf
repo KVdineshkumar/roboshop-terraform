4410a751404efa1ed37bcc081601167fd57c1354
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-16 16:19:23 +0530

    hey

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "test" {
-  source = "https://github.com/KVdineshkumar/tf-module-app.git"
+  source = "git::https://github.com/KVdineshkumar/tf-module-app.git"
 
           # git::https://github.com/KVdineshkumar/tf-module-app.git
   env = "dev"
