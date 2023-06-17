21e563ef6065ee34273d8eac371746cbeb288d9c
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-06-18 00:49:07 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-module "test" {
-  source = "git::https://github.com/KVdineshkumar/tf-module-app.git"
-
+module "instances" {
+  for_each = var.components
+  source   = "git::https://github.com/KVdineshkumar/tf-module-app.git"
+  component     = each.key
   env = var.env
 }
 
