aa17d621e08e9a32504afcd7824d8ab9d4abff55
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-07-11 16:54:09 +0530

    terraform

--- env-dev/main.tf
+++
@@ -1,4 +0,0 @@
-module "test" {
-  source = "git::https://github.com/KVdineshkumar/tf-module-app.git"
-  env    = var.env
-}
--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ module "vpc" {
   source = "git::https://github.com/KVdineshkumar/tf-module-vpc.git"
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
+  web_subnet_cidr_block = each.value["web_subnet_cidr_block"]
+  env = var.env
+  tags = var.tags
 }
