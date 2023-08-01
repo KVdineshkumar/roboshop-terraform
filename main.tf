0f1d9a136356b8238bea0bce987b0bead2609b35
KVdineshkumar <kamisettidineshkumar@gmail.com>
2023-08-01 14:27:10 +0530

    terraform

--- main.tf
+++ main.tf
@@ -34,26 +34,26 @@ module "rabbitmq" {
 
 }
 
-module "rds" {
-  source = "git::https://github.com/KVdineshkumar/tf-module-rds.git"
-  for_each       = var.rds
-  component      = each.value["component"]
-  engine         = each.value["engine"]
-  engine_version = each.value["engine_version"]
-  db_name        = each.value["db_name"]
-  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
-  instance_count = each.value["instance_count"]
-  instance_class = each.value["instance_class"]
-  vpc_id        = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  sg_subnet_cidr= lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets",null), "app", null), "cidr_block", null)
-
-
-
-
-  tags           = var.tags
-  env            = var.env
-  kms_key_arn     = var.kms_key_arn
-}
+#module "rds" {
+#  source = "git::https://github.com/KVdineshkumar/tf-module-rds.git"
+#  for_each       = var.rds
+#  component      = each.value["component"]
+#  engine         = each.value["engine"]
+#  engine_version = each.value["engine_version"]
+#  db_name        = each.value["db_name"]
+#  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+#  instance_count = each.value["instance_count"]
+#  instance_class = each.value["instance_class"]
+#  vpc_id        = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+#  sg_subnet_cidr= lookup(lookup(lookup(lookup(var.vpc, "main", null ), "subnets",null), "app", null), "cidr_block", null)
+#
+#
+#
+#
+#  tags           = var.tags
+#  env            = var.env
+#  kms_key_arn     = var.kms_key_arn
+#}
 module "documentdb" {
   source = "git::https://github.com/KVdineshkumar/tf-module-documentdb.git"
   for_each       = var.documentdb
